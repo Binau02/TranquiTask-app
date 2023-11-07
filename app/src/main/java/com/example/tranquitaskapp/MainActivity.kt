e8cc7facf55eeba1dd029205ddffb629224dc6e7
HugoDenizot <hugo.denizot@hotmail.fr>
2023-11-06 20:28:20 -0500

    ADD- NavigationBar

--- app/src/main/java/com/example/tranquitaskapp/MainActivity.kt
+++ app/src/main/java/com/example/tranquitaskapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.tranquitaskapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tranquitaskapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.leaderboard -> replaceFragment(Leaderboard())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.friends -> replaceFragment(Friends())
+
+                else ->{
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
