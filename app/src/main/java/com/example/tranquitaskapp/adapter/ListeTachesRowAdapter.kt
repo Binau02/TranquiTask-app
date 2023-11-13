86de1d610a87ae22e06afd624960f62b8dc1b9bd
HugoDenizot <hugo.denizot@hotmail.fr>
2023-11-13 16:53:43 -0500

    ajout detail tache

--- app/src/main/java/com/example/tranquitaskapp/adapter/ListeTachesRowAdapter.kt
+++ app/src/main/java/com/example/tranquitaskapp/adapter/ListeTachesRowAdapter.kt
@@ -3,12 +3,13 @@ package com.example.tranquitaskapp.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tranquitaskapp.R
-import com.example.tranquitaskapp.data.LeaderboardModel
 import com.example.tranquitaskapp.data.ListeTachesModel
 
 class ListeTachesRowAdapter(val data: List<ListeTachesModel>) :
@@ -17,6 +18,13 @@ class ListeTachesRowAdapter(val data: List<ListeTachesModel>) :
         val imageView = row.findViewById<ImageView>(R.id.logoImageView)
         val pseudoView = row.findViewById<TextView>(R.id.nameTextView)
         val progressBar = row.findViewById<ProgressBar>(R.id.progressBarFront)
+
+        val cardView = row.findViewById<CardView>(R.id.task_card_view)
+        val taskDuration = row.findViewById<TextView>(R.id.task_duration)
+        val taskDeadline = row.findViewById<TextView>(R.id.task_deadline)
+        val taskPriority = row.findViewById<TextView>(R.id.task_priority)
+        val taskCategory = row.findViewById<TextView>(R.id.task_category)
+        val buttonDevelop = row.findViewById<Button>(R.id.button_develop)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
@@ -30,10 +38,41 @@ class ListeTachesRowAdapter(val data: List<ListeTachesModel>) :
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.buttonDevelop.setOnClickListener{
+            data[position].isDetail = !data[position].isDetail
+            if(holder.buttonDevelop.text == "v"){
+                holder.buttonDevelop.text = "^"
+            }else{
+                holder.buttonDevelop.text = "v"
+            }
+            notifyItemChanged(position) // Informer l'adaptateur du changement
+        }
         holder.pseudoView.text = data[position].name
         holder.imageView.setImageResource(data[position].logoResId)
         holder.progressBar.progress = data[position].progress
+
+        if (data[position].isDetail){
+            holder.cardView.visibility = View.VISIBLE
+        }else{
+            holder.cardView.visibility = View.GONE
+        }
+        holder.cardView.requestLayout()
+        holder.taskDuration.text = convertMinutesToTimeString(data[position].duration)
+        holder.taskDeadline.text = data[position].deadline
+        holder.taskPriority.text = data[position].priority
+        holder.taskCategory.text = data[position].category
     }
 
     override fun getItemCount(): Int = data.size
+
+    fun convertMinutesToTimeString(totalMinutes: Int): String {
+        val hours = totalMinutes / 60
+        val minutes = totalMinutes % 60
+
+        val hoursString = if (hours < 10) "0$hours" else "$hours"
+        val minutesString = if (minutes < 10) "0$minutes" else "$minutes"
+
+        return "$hoursString:$minutesString"
+    }
 }
