50ad9b18e71b20d5b233a154fa9ea9b71227d578
AurelienPaquet <aurelienpaquetpro2021@gmail.com>
2023-11-21 15:52:03 -0500

    PriorityTask

---
+++ app/src/main/java/com/example/tranquitaskapp/fragment/ForgotPassword.kt
@@ -0,0 +1,94 @@
+package com.example.tranquitaskapp.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tranquitaskapp.R
+import com.example.tranquitaskapp.User
+import com.example.tranquitaskapp.firebase.MyFirebase
+import com.example.tranquitaskapp.adapter.FriendsRowAdapter
+import com.example.tranquitaskapp.data.FriendsModel
+import com.example.tranquitaskapp.interfaces.BottomBarVisibilityListener
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// private const val ARG_PARAM1 = "param1"
+// private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ForgotPassword.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ForgotPassword : Fragment() {
+
+
+    private val db = MyFirebase.getFirestoreInstance()
+    private lateinit var rv: RecyclerView
+
+    private var bottomBarListener: BottomBarVisibilityListener? = null
+
+    private lateinit var badge : TextView
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is BottomBarVisibilityListener) {
+            bottomBarListener = context
+        }
+        bottomBarListener?.setBottomBarVisibility(this)
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.connection_password_forgotten, container, false)
+
+
+        return view
+        // Inflate the layout for this fragment
+    }
+    /*
+        companion object {
+            /**
+             * Use this factory method to create a new instance of
+             * this fragment using the provided parameters.
+             *
+             * @param param1 Parameter 1.
+             * @param param2 Parameter 2.
+             * @return A new instance of fragment Friends.
+             */
+            // TODO: Rename and change types and number of parameters
+            @JvmStatic
+            fun newInstance(param1: String, param2: String) =
+                Friends().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+        }
+
+     */
+}
