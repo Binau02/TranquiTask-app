4f4f2486c05759244e5be03da41d22ddcea936c1
HugoDenizot <hugo.denizot@hotmail.fr>
2023-11-22 19:02:05 -0500

    onClick sur la categorie

--- app/src/main/java/com/example/tranquitaskapp/fragment/ListTaches.kt
+++ app/src/main/java/com/example/tranquitaskapp/fragment/ListTaches.kt
@@ -47,7 +47,7 @@ class ListTaches (private val period: Period = Period.ALL, private val category:
 
 
     override fun onStartButtonClick(position: Int) {
-//        val fragment = StartTask(ListeTacheModel[position]) // Remplacez par le fragment que vous souhaitez afficher
+//        val fragment = StartTask(ListTask[position]) // Remplacez par le fragment que vous souhaitez afficher
 //        val transaction = fragmentManager?.beginTransaction()
 //        transaction?.replace(R.id.frameLayout, fragment)?.commit()
     }
