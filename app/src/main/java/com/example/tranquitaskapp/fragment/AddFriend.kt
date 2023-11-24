328bb44403b6160d3e0f0589e84c802ca1ed16f5
Binau02 <aubin.saunier@gmail.com>
2023-11-24 00:07:01 -0500

    add friend

---
+++ app/src/main/java/com/example/tranquitaskapp/fragment/AddFriend.kt
@@ -0,0 +1,59 @@
+package com.example.tranquitaskapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import com.example.tranquitaskapp.R
+import com.example.tranquitaskapp.firebase.MyFirebase
+import android.util.Log
+import android.widget.Toast
+import com.example.tranquitaskapp.data.User
+import com.google.firebase.firestore.FieldValue
+
+
+class AddFriend : Fragment() {
+    private val db = MyFirebase.getFirestoreInstance()
+
+    private fun addFriend(view : View) {
+        val email = view.findViewById<EditText>(R.id.mail).text.toString()
+
+        db.collection("user").whereEqualTo("email", email).get().addOnSuccessListener { documents ->
+            if (documents.documents.isNotEmpty()) {
+                val friend = documents.documents[0].reference
+
+                val test = friend.update("demandes", FieldValue.arrayUnion(User.ref))
+                .addOnFailureListener { e ->
+                    Log.w("Firestore", "Error adding item to the array", e)
+                }
+            }
+            else {
+                val packageName = this.context?.packageName
+
+                val message = getString(resources.getIdentifier("friend_not_exist", "string", packageName))
+                Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        .addOnFailureListener { exception ->
+            // Gérer les erreurs éventuelles
+            Log.e("ERROR", "Erreur lors de la récupération du user : $exception")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_friend, container, false)
+
+        view.findViewById<ImageView>(R.id.search).setOnClickListener {
+            addFriend(view)
+        }
+
+        return view
+    }
+}
