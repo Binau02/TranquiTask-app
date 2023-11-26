e05780d6381be28c64e8ff0d03f2ada15e7a0d06
HugoDenizot <hugo.denizot@hotmail.fr>
2023-11-26 15:06:15 -0500

    Ajout modifyProfil

--- app/src/main/java/com/example/tranquitaskapp/fragment/ModifyProfile.kt
+++ app/src/main/java/com/example/tranquitaskapp/fragment/ModifyProfile.kt
@@ -1,7 +1,9 @@
 package com.example.tranquitaskapp.fragment
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +18,16 @@ import com.bumptech.glide.Glide
 import com.example.tranquitaskapp.R
 import com.example.tranquitaskapp.data.User
 import com.example.tranquitaskapp.firebase.MyFirebase
+import com.example.tranquitaskapp.firebase.MyFirebaseAuth
 import com.example.tranquitaskapp.interfaces.BottomBarVisibilityListener
+import com.google.firebase.firestore.FieldValue
 
 class ModifyProfile : Fragment() {
 
     private val db = MyFirebase.getFirestoreInstance()
+    private val auth = MyFirebaseAuth.getFirestoreInstance()
     private lateinit var rv: RecyclerView
+    private lateinit var sharedPreferences: SharedPreferences
 
     private var bottomBarListener: BottomBarVisibilityListener? = null
 
@@ -32,13 +38,51 @@ class ModifyProfile : Fragment() {
             bottomBarListener = context
         }
         bottomBarListener?.setBottomBarVisibility(this)
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
     }
 
     private fun onClickBack(){
         Toast.makeText(this.context, "Le bouton Retour a été cliqué !", Toast.LENGTH_SHORT).show()
     }
-    private fun onClickSave(){
-        Toast.makeText(this.context, "Le bouton Sauvegarder a été cliqué !", Toast.LENGTH_SHORT).show()
+    private fun onClickSave(newUsername : String, newMail: String, newPassword: String){
+        val currentUser = auth.currentUser
+        currentUser?.updateEmail(newMail)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // L'adresse e-mail a été mise à jour avec succès
+                    // Vous pouvez mettre à jour localement votre interface utilisateur si nécessaire
+                } else {
+                    // Une erreur s'est produite lors de la mise à jour de l'adresse e-mail
+                    // Vous pouvez utiliser task.exception pour obtenir plus d'informations sur l'erreur
+                }
+            }
+
+        currentUser?.updatePassword(newPassword)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Le mot de passe a été mis à jour avec succès
+                    // Vous pouvez mettre à jour localement votre interface utilisateur si nécessaire
+                } else {
+                    // Une erreur s'est produite lors de la mise à jour du mot de passe
+                    // Vous pouvez utiliser task.exception pour obtenir plus d'informations sur l'erreur
+                }
+            }
+
+        val userRef = db.collection("user").document(User.id)
+        userRef.update("username", FieldValue.arrayUnion(newUsername))
+            .addOnSuccessListener {
+                // La mise à jour a réussi
+                Log.d("Update", "ID de la tâche ajouté au tableau tasks de l'utilisateur")
+            }
+            .addOnFailureListener { e ->
+                // Gérer les erreurs lors de la mise à jour
+                Log.e("Update", "Erreur lors de l'ajout de l'ID de la tâche : $e")
+            }
+        User.username = newUsername
+        User.mail = newMail
+        val fragment = Profile()
+        val transaction = fragmentManager?.beginTransaction()
+        transaction?.replace(R.id.frameLayout, fragment)?.commit()
     }
     private fun onClickCancel(){
         Toast.makeText(this.context, "Le bouton Annuler a été cliqué !", Toast.LENGTH_SHORT).show()
@@ -62,6 +106,10 @@ class ModifyProfile : Fragment() {
         val passwordConfirm = view.findViewById<EditText>(R.id.reg_passwordconfirm)
 
         pseudo.text = User.username
+        username.setText(User.username)
+        email.setText(User.mail)
+        password.setText(sharedPreferences.getString("password", null))
+        passwordConfirm.setText(sharedPreferences.getString("password", null))
 
         if (User.profile_picture != "") {
             Glide.with(this)
@@ -73,7 +121,11 @@ class ModifyProfile : Fragment() {
             onClickBack()
         }
         buttonSave.setOnClickListener {
-            onClickSave()
+            if(password.text.toString()==passwordConfirm.text.toString()){
+                onClickSave(username.text.toString(),email.text.toString(),password.text.toString())
+            }else{
+                Toast.makeText(this.context, "Les mots de passes ne sont pas identiques", Toast.LENGTH_SHORT).show()
+            }
         }
         buttonCancel.setOnClickListener {
             onClickCancel()
